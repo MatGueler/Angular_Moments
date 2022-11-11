65096994710708e4f016add02505887f36a6bf6c
MattGueler <mateus.gueler@gmail.com>
2022-11-11 17:27:56 -0300

    :pencil: feat:
    Create a layout to footer component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,10 +4,12 @@ import { RouterModule, Routes } from '@angular/router';
 
 // * Components
 import { AboutComponent } from './components/pages/about/about.component';
+import { AddMomentComponent } from './components/pages/add-moment/add-moment.component';
 import { HomeComponent } from './components/pages/home/home.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
+  { path: 'new', component: AddMomentComponent },
   { path: 'about', component: AboutComponent },
 ];
 
